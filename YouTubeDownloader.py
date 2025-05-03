3e54255894f2caed2fa7e4c8018f4903a6645efb
MDShuvo721 <abidahassan420@gmail.com>
2025-05-03 21:10:39 +0600

    2nd push

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -17,20 +17,24 @@ yt = pytube.YouTube(url)
 
 path = Path("D:/YouTube")
 
-if yt.length <= 600:
-    stream = yt.streams.filter(res="1080p", progressive=True, file_extension="mp4").first()
-    print("Video is short, selecting 1080p resolution.")
-else:
-    stream = yt.streams.filter(res="720p", progressive=True, file_extension="mp4").first()
-    print("Video is long, selecting 720p resolution.")
+# if yt.length <= 600:
+#     stream = yt.streams.filter(res="1080p", progressive=True, file_extension="mp4").first()
+#     print("Video is short, selecting 1080p resolution.")
+# else:
+#     stream = yt.streams.filter(res="720p", progressive=True, file_extension="mp4").first()
+#     print("Video is long, selecting 720p resolution.")
 
-# If 1080p or 720p aren't available, select the highest resolution available
-if not stream:
-    stream = yt.streams.get_highest_resolution()
+# # If 1080p or 720p aren't available, select the highest resolution available
+# if not stream:
+#     stream = yt.streams.get_highest_resolution()
 
-# Sanitize the filename to ensure it's valid
-filename = sanitize_filename(f"{yt.title}.mp4")
+# # Sanitize the filename to ensure it's valid
+# filename = sanitize_filename(f"{yt.title}.mp4")
 
-stream.download(output_path=path, filename=filename)
+# stream.download(output_path=path, filename=filename)
+
+audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+
+audio_stream.download(output_path=path)
 
 print(f"{yt.title} has downloaded to {path}")
